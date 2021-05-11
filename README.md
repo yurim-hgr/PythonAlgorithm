f971b88caa5cd3a6c2c332bf2c032aefa1c4e302
rimpaca <>
2021-05-11 04:24:41 +0000

    6080 python

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 코드업 기초100제 python 
 
 repl.it 커밋
+
+### 깃 저장소 초기화
+git init
+
+### 깃 저장소에 모든 파일 추가
+git add .
+
+### 커밋할때 코멘트 추가
+git commit -m 'test'
+
+### 깃 저장소에 올리기
+git push origin master
---
+++ codeup/ex1.py
@@ -0,0 +1,23 @@
+
+#a = input()
+#a, b, c = map(int , input().split())
+#n = ord(input())
+
+#6080  [기초-종합] 주사위 2개 던지기(설명)(py)
+n,m = map(int,input().split())
+#m = int(input())
+for i in range(1, n+1) :
+  for j in range(1, m+1) :
+
+    print(i,j)
+    
+  
+# n =
+# s = 0
+# for i in range(1, n+1) :
+#   if i%2==0 :
+#     s += i
+
+# print(s)
+  
+
