ca2abc48eecf3312acfc4b6a60d2f24f1a65987d
rimpaca <>
2021-05-30 04:33:51 +0000

    코드업 기초100제 6097

--- codeup/ex1.py
+++ codeup/ex1.py
@@ -1,4 +1,68 @@
 
+#6097 : [기초-리스트] 설탕과자 뽑기(py)
+h , w = map(int, input().split())
+data= []
+for i in range(h+1):
+  data.append([])
+  for j in range(w+1): 
+    data[i].append(0)
+
+n = int(input())
+for i in range(n):
+  l,d,x,y = input().split()
+  l = int(l)
+  d = int(d)
+  x = int(x)
+  y = int(y)
+  for j in range(l):
+    if d== 0 :      
+      data[x][y+j]=1
+    else :
+      data[x+j][y] = 1
+      
+for i in range(1,h+1):
+  for j in range(1, w+1):
+    print(data[i][j], end= ' ')
+  print()
+
+
+#6096 : [기초-리스트] 바둑알 십자 뒤집기(py)
+
+data= []
+
+for i in range(20):
+  data.append([])
+  for j in range(20): 
+    data[i].append(0)
+
+for i in range(19):
+  a = input().split()
+  for j in range(19):
+    data[i+1][j+1] = int(a[j])
+
+
+n = int(input())
+for i in range(n):
+  a, b = input().split()
+  for j in range(1, 20):
+    
+    if data[j][int(b)] ==0:
+      data[j][int(b)] = 1
+    else :
+      data[j][int(b)] = 0
+      
+    if data[int(a)][j] == 0:
+      data[int(a)][j]=1
+    else :
+      data[int(a)][j]=0
+
+
+for i in range(1,20):
+  for j in range(1, 20):
+    print(data[i][j], end= ' ')
+  print()
+
+
 #책(이코테) 그리디 - 더하기 혹은 곱하기 (그리디:2문제)
 
 s = input()
--- codeup/ex2.py
+++ codeup/ex2.py
@@ -1,3 +1,4 @@
+
 #a = input()
 #a, b, c = map(int , input().split())
 #n = ord(input())
@@ -6,20 +7,32 @@
 #n = input()
 #a=ord(input())
 
-#책(이코테) 그리디 - 더하기 혹은 곱하기 
+#6097 : [기초-리스트] 설탕과자 뽑기(py)
+h , w = map(int, input().split())
+data= []
+for i in range(h+1):
+  data.append([])
+  for j in range(w+1): 
+    data[i].append(0)
 
-s = input()
-result = 0
+n = int(input())
+for i in range(n):
+  l,d,x,y = input().split()
+  l = int(l)
+  d = int(d)
+  x = int(x)
+  y = int(y)
+  for j in range(l):
+    if d== 0 :      
+      data[x][y+j]=1
+    else :
+      data[x+j][y] = 1
+      
+for i in range(1,h+1):
+  for j in range(1, w+1):
+    print(data[i][j], end= ' ')
+  print()
 
-for i in range (len(s)) :
-  num = int(s[i])
-  if num <= 1 or result <= 1:
-    result = result + num
-
-  else :
-    result = result * num
-
-print(result)
 
 #for i in range(n+1, -1 , -1) :  #카운트한 값을 공백을 두고 출력
  #print(a[i], end=' ')
